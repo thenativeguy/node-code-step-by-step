f2d1d6f54310c58d26259e2d24ffeab12d0d140e
thenativeguy <xeroseven44@gmail.com>
2025-02-13 00:06:20 +0500

    getting input from command line

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const http = require('http');
+const fileOps = require('./getting-input-from-cmdline')
 
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'application/json'});
-    res.write(JSON.stringify({name: 'Kamran', age: 29}))
-    res.end();
-}).listen(3000)
+const input = process.argv;
+
+let command = input[2]
+let fileName = input[3]
+// createFile(input[2], input[3])
+if(command === 'add'){
+    let content = input[4]
+    fileOps.createFile(fileName, content)
+}else{
+
+    fileOps.removeFile(fileName)
+}
+// http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type':'application/json'});
+//     res.write(JSON.stringify({name: 'Kamran', age: 29}))
+//     res.end();
+// }).listen(3000)
